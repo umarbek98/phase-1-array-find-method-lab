a6411d8d28fc98c780ca9ce9893b0f96ffb132dd
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-01-13 13:02:03 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+function superbowlWin(record){
+    const winner = record.find(({ result }) => result === "W")
+    if (winner === undefined) {
+        return undefined
+    }else {
+        return winner.year
+    }
+}
+console.log(superbowlWin(record))
